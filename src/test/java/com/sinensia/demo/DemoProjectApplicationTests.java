854afad00fbdd226dca7c9e9957b467878207b7b
Raul Cami <raulcami01@hotmail.com>
2022-01-27 12:47:11 +0100

    'Added rootTest'

--- src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.sinensia.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class DemoProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void rootTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/", String.class))
+				.isEqualTo("Esta es la página de incio");
+	}
 }

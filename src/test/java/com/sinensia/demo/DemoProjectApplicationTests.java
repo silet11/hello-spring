2a47a065fab02c2f9f7fb959387dcefec2c44176
Raul Cami <raulcami01@hotmail.com>
2022-01-28 13:37:29 +0100

    'Added more Tests'

--- src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
@@ -1,6 +1,8 @@
 package com.sinensia.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class DemoProjectApplicationTests {
-
-	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
@@ -46,4 +46,44 @@ class DemoProjectApplicationTests {
 				.isEqualTo("3");
 	}
 
+	@Test
+	void canAddZero(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=0&b=2", String.class))
+				.isEqualTo("2");
+	}
+
+	@Test
+	void canAddNegative(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=-2", String.class))
+				.isEqualTo("-1");
+	}
+
+	@Test
+	void canAddNullA(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a&b=2", String.class))
+				.isEqualTo("2");
+	}
+
+	@Test
+	void canAddNullB(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=", String.class))
+				.isEqualTo("1");
+	}
+
+	@Test
+	void helloNames(@Autowired TestRestTemplate restTemplate) {
+		String[] arr = {"Raul","Cami"};
+		for(String name:arr){
+			assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+					.isEqualTo("Hello "+name+"!");
+		}
+	}
+
+	@Autowired TestRestTemplate restTemplate;
+	@ParameterizedTest
+	@ValueSource(strings={"Raul","Cami"})
+	void helloParamNames(String name) {
+			assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+					.isEqualTo("Hello "+name+"!");
+	}
 }

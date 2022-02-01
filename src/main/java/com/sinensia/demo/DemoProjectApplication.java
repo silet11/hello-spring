a7f23782fc71f4806a9fe46a4ab7955a6fdf957d
Raul Cami <raulcami01@hotmail.com>
2022-02-01 09:51:44 +0100

    'Added substract  divide'

--- src/main/java/com/sinensia/demo/DemoProjectApplication.java
+++ src/main/java/com/sinensia/demo/DemoProjectApplication.java
@@ -6,34 +6,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Generated;
 
 
 @SpringBootApplication
 @RestController
 public class DemoProjectApplication {
 
+	@Generated(value = "org.springframework.boot")
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
-    }
-    @GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
+	}
+
+	@GetMapping("/hello")
+
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 
 	}
+
 	@GetMapping("/")
-	public String paginaincio(){
+	public String paginaincio() {
 		return String.format("Esta es la página de incio");
 
 	}
 
 	@GetMapping("/add")
 	public Object add(
-			@RequestParam(value="a", defaultValue = "0") Float a,
-			@RequestParam(value="b", defaultValue = "0") Float b
+			@RequestParam(value = "a", defaultValue = "0") Float a,
+			@RequestParam(value = "b", defaultValue = "0") Float b
 	) {
-		Float sum = a+b;
+		Float sum = a + b;
 		Float decimals = sum - sum.intValue();
-		if(decimals!=0) {
+		if (decimals != 0) {
 			return sum;
 		}
 		return Integer.valueOf(sum.intValue());
@@ -41,16 +46,43 @@ public class DemoProjectApplication {
 
 	@GetMapping("/multiply")
 	public Object multiply(
-			@RequestParam(value="a", defaultValue = "0") Float a,
-			@RequestParam(value="b", defaultValue = "0") Float b
+			@RequestParam(value = "a", defaultValue = "0") Float a,
+			@RequestParam(value = "b", defaultValue = "0") Float b
 	) {
-		Float product = a*b;
+		Float product = a * b;
 		Float decimals = product - product.intValue();
-		if(decimals!=0) {
+		if (decimals != 0) {
 			return product;
 		}
 		return Integer.valueOf(product.intValue());
 	}
+
+	@GetMapping("/subtract")
+	public Object subtract(
+			@RequestParam(value="a", defaultValue = "0") Float a,
+			@RequestParam(value="b", defaultValue = "0") Float b
+	) {
+		Float sub = a - b;
+		Float decimals = sub - sub.intValue();
+		if(decimals!=0) {
+			return sub;
+		}
+		return Integer.valueOf(sub.intValue());
+	}
+
+	@GetMapping("/divide")
+	public Object divide(
+			@RequestParam(value="a", defaultValue = "0") Float a,
+			@RequestParam(value="b", defaultValue = "0") Float b
+	) {
+		Float division = a / b;
+		Float decimals = division - division.intValue();
+		if(decimals!=0) {
+			return division;
+		}
+		return Integer.valueOf(division.intValue());
+	}
+
 
 
 }
